12d1d09f2a0be29ef7778242242270197743ab95
XenosRbel <delldd@yandex.by>
2021-02-27 19:03:45 +0300

    Добавить авторизацию.

--- tire-service/app/controllers/application_controller.rb
+++ tire-service/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  private
+
+  def after_sign_in_path_for(_resource)
+    current_user_path
+  end
+
+  def after_sign_out_path_for(_resource_or_scope)
+    request.referrer
+  end
 end
--- tire-service/config/routes.rb
+++ tire-service/config/routes.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  devise_for :users, class_name: "Admin::User"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "home#index"
+
+  get "persons/profile", as: "user_root"
 end
