61691979753894454310dc6c494c43e7c34077c0
XenosRbel <delldd@yandex.by>
2021-02-27 19:48:10 +0300

    Настроить Puma.

--- tire-service/config/puma.rb
+++ tire-service/config/puma.rb
@@ -1,37 +1,32 @@
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
+# frozen_string_literal: true
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+workers ENV.fetch("WEB_CONCURRENCY", 2).to_i
 
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+max_threads = ENV.fetch("PUMA_MAX_THREADS", 16).to_i
+min_threads = ENV.fetch("PUMA_MIN_THREADS", max_threads).to_i
+threads min_threads, max_threads
 
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+preload_app!
+activate_control_app "tcp://127.0.0.1:9000", no_token: true
 
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked webserver processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+puma_host = ENV["PUMA_HOST"]
+puma_port = ENV.fetch("PORT", 3000).to_i
 
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
+if puma_host && !puma_host.empty?
+  bind "tcp://#{puma_host}:#{puma_port}"
+else
+  port puma_port
+end
 
-# Allow puma to be restarted by `rails restart` command.
+environment ENV["RACK_ENV"] || "development"
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
+
+#pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
+end
+
 plugin :tmp_restart
