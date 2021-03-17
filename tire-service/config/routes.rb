ffd287dab9099b3eae0bb078abb9ed5154822b47
XenosRbel <delldd@yandex.by>
2021-03-17 19:35:41 +0300

    Добавить фронтовую страницу

--- tire-service/config/routes.rb
+++ tire-service/config/routes.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  namespace :front do
+    get "home/index"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  root to: "front/home#index"
 end
