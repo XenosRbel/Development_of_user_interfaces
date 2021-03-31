33326c124c7b240f47119955f0d5f0d63814996b
XenosRbel <delldd@yandex.by>
2021-03-31 14:43:13 +0300

    Добавить изменение скидок

--- tire-service/app/controllers/admin/discounts_controller.rb
+++ tire-service/app/controllers/admin/discounts_controller.rb
@@ -2,6 +2,9 @@
 
 module Admin
   class DiscountsController < ApplicationController
+    load_and_authorize_resource
+    skip_load_resource only: :index
+
     datatable model: Discount, exclude_attributes: %w[created_at updated_at]
 
     def index
