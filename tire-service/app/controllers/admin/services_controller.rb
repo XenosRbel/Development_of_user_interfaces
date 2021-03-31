01f952d414b060ab51cff7de7c6bc1c54e72b7ac
XenosRbel <delldd@yandex.by>
2021-03-31 14:57:37 +0300

    Добавить изменение услуг

--- tire-service/app/controllers/admin/services_controller.rb
+++ tire-service/app/controllers/admin/services_controller.rb
@@ -2,6 +2,9 @@
 
 module Admin
   class ServicesController < ApplicationController
+    load_and_authorize_resource
+    skip_load_resource only: :index
+
     datatable model: Service, exclude_attributes: %w[created_at updated_at]
 
     def index
