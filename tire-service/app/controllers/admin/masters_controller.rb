a039c59ff539d68d0bffd9c18031f1c5be88c83e
XenosRbel <delldd@yandex.by>
2021-03-31 14:54:32 +0300

    Добавить изменение мастеров

--- tire-service/app/controllers/admin/masters_controller.rb
+++ tire-service/app/controllers/admin/masters_controller.rb
@@ -2,6 +2,9 @@
 
 module Admin
   class MastersController < ApplicationController
+    load_and_authorize_resource
+    skip_load_resource only: :index
+
     datatable model: Master, exclude_attributes: %w[created_at updated_at]
     def index
       super
