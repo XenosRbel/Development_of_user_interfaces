897ca3fcc387c6abef0d92612899e35c6556802c
XenosRbel <delldd@yandex.by>
2021-03-31 23:02:53 +0300

    Исправить миграции

--- tire-service/db/migrate/20210322171234_add_percent_and_description_and_title_and_start_date_and_end_date_to_admin_discounts.rb
+++ tire-service/db/migrate/20210322171234_add_percent_and_description_and_title_and_start_date_and_end_date_to_admin_discounts.rb
@@ -4,8 +4,8 @@ class AddPercentAndDescriptionAndTitleAndStartDateAndEndDateToAdminDiscounts < A
       add_column :admin_discounts, :percent, :integer, default: 0
       add_column :admin_discounts, :description, :string, null: false, default: ""
       add_column :admin_discounts, :title, :string, null: false, default: ""
-      add_column :admin_discounts, :start_date_at, :datetime, null: false, default: Time.now.utc
-      add_column :admin_discounts, :end_date_at, :datetime, null: false, default: Time.now.utc
+      add_column :admin_discounts, :start_date_at, :datetime, null: false
+      add_column :admin_discounts, :end_date_at, :datetime, null: false
     end
   end
 end
